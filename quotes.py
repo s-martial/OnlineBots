7a7b266ce2df585f6ca74fda4f221488f9caa475
Stephane <martial.stephane.c@gmail.com>
2020-05-03 17:31:57 +0200

    Update

--- quotes.py
+++ quotes.py
@@ -1,36 +1,43 @@
 quote = [
-' **“Live as if you were to die tomorrow. Learn as if you were to live forever.” **— Mahatma Gandhi',
+    ' ** “Live as if you were to die tomorrow. Learn as if you were to live forever.” **— Mahatma Gandhi',
+    ' ** “[Positive] change is the end result of all true learning.” ** --Leo Buscaglia',
+    ' ** “The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn.” ** — Alvin Toffler',
+    ' ** “Mental models are to your brain as apps are to your smartphone.” **  — Jayme Hoffman',
+    ' ** “Education is not the learning of facts, but training the mind to think.” **  — Albert Einstein',
+    ' ** "If you’re going through hell, keep going." **  - Churchill',
+    ' ** “Focus on signal over noise. Don’t waste time on stuff that doesn’t actually make things better.” **  - Elon Musk ',
+    ' ** “I think it’s very important to have a feedback loop, where you’re constantly thinking about what you’ve done and how you could be doing it better. I think that’s the single best piece of advice: constantly think about how you could be doing things better and questioning yourself.” **  - Elon Musk ',
+    ' ** “Don’t tell me what you like, tell me what you don’t like.” **  - Elon Musk ',
+    ' ** “You look at the fundamentals and construct your reasoning from that and then see if you have a conclusion that works or doesn’t work. And it may or may not be different from what people have done in the past. It’s harder to think that way, though.” **  - Elon Musk ',
+    ' ** “People work better when they know what the goal is and why. It is important that people look forward to coming to work in the morning and enjoy working.” **  - Elon Musk ',
+    ' ** “The first step is to establish that something is possible; then probability will occur.” **  - Elon Musk ',
+    ' ** “I say something, and then it usually happens. Maybe not on schedule, but it usually happens.” **  - Elon Musk ',
+    ' ** “You should take the approach that you’re wrong. Your goal is to be less wrong.” **  - Elon Musk ',
+    ' ** “Failure is an option here. If things are not failing, you are not innovating enough.” **  - Elon Musk ',
+    ' ** “It is important to view knowledge as sort of a semantic tree — make sure you understand the fundamental principles, ie the trunk and big branches, before you get into the leaves/details or there is nothing for them to hang on to.” **  - Elon Musk ',
+    ' ** “I think it is possible for ordinary people to choose to be extraordinary.” **  - Elon Musk ',
+    ' ** “The world wants to assign you a role. The minute you accept that role? You’re doomed” **  — Robert Greene',
+    ' ** “Money is made by sitting, not trading.” ** — Jesse Livermore ',
+    ' ** “Letting losses run is the most serious mistake made by most investors.” ** — William O’Neil ',
+    ' ** “The elements of good trading are: (1) cutting losses, (2) cutting losses, and (3) cutting losses. If you can follow these three rules, you may have a chance.” ** — Ed Seykota ',
+    ' ** “Frankly, I don’t see markets; I see risks, rewards, and money.” ** — Larry Hite ',
+    ' ** “When I became a winner, I said, ‘I figured it out, but if I’m wrong, I’m getting the hell out, because I want to save my money and go on to the next trade.’” ** — Marty Schwartz ',
+    ' ** “I always define my risk, and I don’t have to worry about it.”  ** — Tony Saliba ',
+    ' ** “I think investment psychology is by far the more important element, followed by risk control, with the least important consideration being the question of where you buy and sell.”  ** — Tom Basso ',
+    ' ** “Learn to take losses. The most important thing in making money is not letting your losses get out of hand.”  ** — Marty Schwartz ',
+    ' ** ”The goal of a successful trader is to make the best trades. Money is secondary.” ** — Alexander Elder ',
+    ' ** “In this business if you’re good, you’re right six times out of ten. You’re never going to be right nine times out of ten.” ** — Peter Lynch ',
+    ' ** “What seems too high and risky to the majority generally goes higher and what seems low and cheap generally goes lower.” ** — William O’Neil ',
+    ' ** “It takes 20 years to build a reputation and 5 minutes to ruin it. If you think about that, you’ll do things differently.”  ** — Warren Buffett ',
+    ' ** “In investing, what is comfortable is rarely profitable.”  ** — Robert Arnott ',
+    ' ** “I’m always thinking about losing money as opposed to making money. Don’t focus on making money, focus on protecting what you have” ** — Paul Tudor Jones ',
+    ' ** “If you personalize losses, you can’t trade.”  ** — Bruce Kovner ',
+    ' ** “Don’t focus on making money; focus on protecting what you have.”  ** — Paul Tudor Jones ',
+    ' ** “Markets are constantly in a state of uncertainty and flux and money is made by discounting the obvious and betting on the unexpected. “ ** — George Soros ',
+    ' ** “Every battle is won or lost before it’s ever fought” ** — Sun Tzu ',
+    ' ** “The game taught me the game. And it didn’t spare me the rod while teaching.” ** — Jesse Livermore ',
+    ' ** “Risk comes from not knowing what you’re doing.” ** — Warren Buffett ',
+    ' ** “The biggest risk is not taking a risk. In a world that’s changing really quickly, the only strategy that is guaranteed to fail is not taking risks.” ** — Mark Zuckerberg ',
+    ' ** “It is not the strongest or the most intelligent who will survive but those who can best manage change.” ** — Charles Darwin '
 
-' **“[Positive] change is the end result of all true learning.” ** --Leo Buscaglia',
-
-' ** “The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn.” ** — Alvin Toffler',
-
-'“Mental models are to your brain as apps are to your smartphone.” — Jayme Hoffman',
-
-'“Education is not the learning of facts, but training the mind to think.” — Albert Einstein',
-
-'"If you’re going through hell, keep going." - Churchill',
-
-' “Focus on signal over noise. Don’t waste time on stuff that doesn’t actually make things better.” - Elon Musk ',
-
-' “I think it’s very important to have a feedback loop, where you’re constantly thinking about what you’ve done and how you could be doing it better. I think that’s the single best piece of advice: constantly think about how you could be doing things better and questioning yourself.” - Elon Musk ',
-
-' “Don’t tell me what you like, tell me what you don’t like.” - Elon Musk ',
-
-' “You look at the fundamentals and construct your reasoning from that and then see if you have a conclusion that works or doesn’t work. And it may or may not be different from what people have done in the past. It’s harder to think that way, though.” - Elon Musk ',
-
-' “People work better when they know what the goal is and why. It is important that people look forward to coming to work in the morning and enjoy working.” - Elon Musk ',
-
-' “The first step is to establish that something is possible; then probability will occur.” - Elon Musk ',
-
-' “I say something, and then it usually happens. Maybe not on schedule, but it usually happens.” - Elon Musk ',
-
-' “You should take the approach that you’re wrong. Your goal is to be less wrong.” - Elon Musk ',
-
-' “Failure is an option here. If things are not failing, you are not innovating enough.” - Elon Musk ',
-
-' “It is important to view knowledge as sort of a semantic tree — make sure you understand the fundamental principles, ie the trunk and big branches, before you get into the leaves/details or there is nothing for them to hang on to.” - Elon Musk ',
-
-' “I think it is possible for ordinary people to choose to be extraordinary.” - Elon Musk ',
-
-' “The world wants to assign you a role. The minute you accept that role? You’re doomed” — Robert Greene' ]
+    ]
